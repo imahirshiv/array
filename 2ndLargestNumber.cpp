51b3959084c46a4585bbaa0a8bb79c1f7df4f318
Ahir Shiv <imahirshiv@gmail.com>
2022-12-12 22:58:38 +0530

    Examples

---
+++ 2ndLargestNumber.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int array[8]={1,4,3,5,6,8,9,2};
+   
+
+    // int ans=0;
+    for (int i = 0; i <8; i++)
+    {
+        cout<<"The members present in array are-:"<<array[i]<<" "<<endl;
+    }
+
+    
+
+    return 0;
+}
---
+++ ifelse.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int savings;
+    cin>>savings;
+
+    if (savings<5000)
+    {
+        cout<<"Neha ";
+    }
+    else
+    {
+        cout<<"Pooja";
+    }
+    cout<<endl;
+
+    return 0;
+}
