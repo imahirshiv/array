1f237c123b443d94798f4c1a2631171f210e6b7b
Ahir Shiv <imahirshiv@gmail.com>
2022-12-12 23:04:49 +0530

    initianl ocnmit

---
+++ elseif.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int savings;
+    cin >> savings;
+
+    if (savings > 5000)
+    {
+        cout << "Neha ";
+    }
+    else if (savings > 2000)
+    {
+        cout << "Rashmi Singh";
+    }
+    else
+    {
+        cout << "Friends";
+    }
+    cout<<endl;
+    return 0;
+}
