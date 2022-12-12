13c80b71e780df62bb30954adc0b02c56a780024
Ahir Shiv <imahirshiv@gmail.com>
2022-12-12 23:11:10 +0530

    sf

---
+++ maximux.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int a,b,c;
+    cin>>a>>b>>c;
+
+    if (a>b)
+    {
+        cout<<"A is greater than B";
+
+    }
+    else if (b>c)
+    {
+        cout<<"B is greater than C";
+    }
+    else
+    {
+        cout<<"C is Grater than All";
+    }
+    
+    
+
+    return 0;
+}
