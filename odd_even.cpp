3c7dff586e0145031057648a3024bc432193f32b
Ahir Shiv <imahirshiv@gmail.com>
2022-12-13 06:17:02 +0530

    inital commit

---
+++ odd_even.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int inpt;
+    cin>>inpt;
+    if (inpt%2==0)
+    {
+        cout<<"Given number is even";
+    }
+    else
+    {
+        cout<<"Given number is odd";
+    }
+    cout<<endl;
+    
+
+    return 0;
+
+}
